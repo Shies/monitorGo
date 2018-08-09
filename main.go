242531eddb653436206e293be636366122a07429
辜凯 <gukai@yupaopao.cn>
2018-08-09 16:42:39 +0800

    update

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"monitorGo/task"
 	"monitorGo/controller"
-	"monitorGo/dao"
 )
 
 func main() {
-
-	tasks := dao.New().TaskList(dao.TASK_BY_ALL, "1")
-	ips := dao.New().TaskIP(dao.IPS_BY_ALL, 1)
-	for _, v := range tasks {
-		task.Request(v, ips[v.Id])
-	}
-
 	controller.Register()
 }
+
+
--- service/service.go
+++ service/service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"monitorGo/conf"
 	"monitorGo/dao"
+	"monitorGo/task"
+	"time"
 )
 
 // Service biz service def.
@@ -17,9 +19,21 @@ func New(c *conf.Config) (s *Service) {
 		c:         c,
 		dao:       dao.New(),
 	}
+
+	go s.asyncTask();
 	return s
 }
 
+func (s *Service) asyncTask() {
+	for {
+		tasks := s.dao.TaskList(dao.TASK_BY_ALL, "1")
+		ips := s.dao.TaskIP(dao.IPS_BY_ALL, 1)
+		for _, v := range tasks {
+			task.Request(v, ips[v.Id])
+		}
+		time.Sleep(time.Duration(60) * time.Second)
+	}
+}
 
 func (s *Service) Close() {
 	return
